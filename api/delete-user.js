cecfa5fe4685ab494f8bcfa12ccd200562ae8b2e
GroveSigilOfficial <grovesigilofficial@gmail.com>
2026-01-24 22:14:09 -0800

    Update delete-user.js

--- api/delete-user.js
+++ api/delete-user.js
@@ -23,7 +23,6 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ message: 'User deleted' });
   } catch (err) {
-    // ALWAYS return JSON
     return res.status(500).json({ error: err?.message || 'Server error' });
   }
 }

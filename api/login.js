410cda31047330ba0a20f649f5a9dfd70fd60efc
GroveSigilOfficial <grovesigilofficial@gmail.com>
2026-01-24 22:13:45 -0800

    Update login.js

--- api/login.js
+++ api/login.js
@@ -18,7 +18,6 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ message: 'Login successful', user: data.user });
   } catch (err) {
-    // ALWAYS return JSON
     return res.status(500).json({ error: err?.message || 'Server error' });
   }
 }
